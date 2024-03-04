b26d6aa864b0739ab2a3f3bbe84eea269015554c
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-03-04 11:28:56 +0100

    create hitTest

--- src/test/java/e1/KnightTest.java
+++ src/test/java/e1/KnightTest.java
@@ -1,6 +1,10 @@
 package e1;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KnightTest {
 
@@ -11,5 +15,11 @@ public class KnightTest {
     public void beforeEach(){
         position = new Pair<>(2,2);
         knight = new KnightImpl(position);
+    }
+
+    @Test
+    public void hitTest(){
+        assertTrue(knight.hit(position.getX(), position.getY()));
+        assertFalse(knight.hit(4, 4));
     }
 }
