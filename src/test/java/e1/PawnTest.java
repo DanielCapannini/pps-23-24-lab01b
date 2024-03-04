e34dde60e88007cec165587534e6fd863d8e000c
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-03-04 11:16:54 +0100

    create hitPositionTest

--- src/test/java/e1/PawnTest.java
+++ src/test/java/e1/PawnTest.java
@@ -1,6 +1,9 @@
 package e1;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PawnTest {
 
@@ -11,5 +14,11 @@ public class PawnTest {
     public void beforEach(){
         position = new Pair<>(2,2);
         pawn = new PawnImpl(position);
+    }
+
+    @Test
+    public void hitPositionTest(){
+        assertTrue(pawn.hit(position.getX(), position.getY()));
+        assertFalse(pawn.hit(5, 5));
     }
 }
