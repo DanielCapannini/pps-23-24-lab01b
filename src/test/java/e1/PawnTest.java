5a38b0952eddbf06e00f0992d296413e40a415fc
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-03-04 11:11:30 +0100

    create PawnImpl

--- src/test/java/e1/PawnTest.java
+++ src/test/java/e1/PawnTest.java
@@ -1,8 +1,13 @@
 package e1;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class PawnTest {
 
     Pawn pawn;
 
-
+    @BeforeEach
+    public void beforEach(){
+        pawn = new PawnImpl();
+    }
 }
